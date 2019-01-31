4e6ab52597286d1610122a5c2edc4c3c2ed17f0e
Brent Perry <contact@brentallan.me>
2019-01-30 19:38:44 -0600

    tried adding mstest

---
+++ Calendar.Tests/ModelTests/LeapYearTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace Calendar.TestTools
+{
+    [TestClass]
+    public class LeapYearTest
+    {
+        [TestMethod]
+        public void IsLeapYear_NumberDivisibleByFour_True()
+        {
+            LeapYear testLeapYear = new LeapYear();
+            Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+        }
+    }
+}
